cadf1990582042d6c065510bad835c82b3f79d0c
devc4sh <plra@kakao.com>
2022-01-01 18:46:58 +0900

    edit

--- test_tf/tf_cloud_test/vpc/provider.tf
+++ test_tf/tf_cloud_test/vpc/provider.tf
@@ -2,8 +2,8 @@ variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY_ID" {}
 
 provider "aws" {
-  access_key = "./tfvars"
-  secret_key = var.AWS_SECRET_ACCESS_KEY_ID
+  access_key = data.vault_aws_access_credentials.tfc.access_key
+  secret_key = data.vault_aws_access_credentials.tfc.secret_key
   region     = ap-northeast-2
 }
 
