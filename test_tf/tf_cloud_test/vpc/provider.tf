53f1824049e2bee45fc1e67eb942336bf4279806
devc4sh <plra@kakao.com>
2022-01-01 18:38:26 +0900

    edit

--- test_tf/tf_cloud_test/vpc/provider.tf
+++ test_tf/tf_cloud_test/vpc/provider.tf
@@ -1,3 +1,8 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
 provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = ap-northeast-2
 }
